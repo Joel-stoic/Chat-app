578c1d254406dcf2f616b3b5165d2d7d0e41ad41
Joel-stoic <joelimmanual.off@gmail.com>
2025-09-14 22:11:09 +0530

    Created server,routes,controllers

---
+++ backend/src/index.js
@@ -0,0 +1,42 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import path from "path";
+
+import { connectDB } from "./config/db.js";
+import authRoutes from "./routes/auth.routes.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
+
+// Middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+// Routes
+app.use("/api/auth", authRoutes);
+
+// Serve frontend in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+// Start server
+app.listen(PORT, () => {
+  console.log("✅ Server is running on PORT:", PORT);
+  connectDB();
+});
